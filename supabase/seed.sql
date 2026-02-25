37e582033c20d06a0beb1c82a32ba12c5e920265
Nikita Malyeyev <maleev2017@ukr.net>
2026-02-25 19:46:20 +0200

    asd

---
+++ supabase/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO storage.buckets (id, name, public)
+VALUES
+  ('tg_media', 'tg_media', true),
+  ('user_avatars', 'user_avatars', true),
+  ('comment_media', 'comment_media', true)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.authors (author_name, author_link, tg_author_id)
+VALUES
+  ('КРИПТО ИЛЬЯ', 'https://t.me/crypto_ilya', -1001579090675),
+  ('COIN 22', 'https://t.me/COIN22T', -1001792822445)
+ON CONFLICT DO NOTHING;
